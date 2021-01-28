8aa5d0ef2b2d0ac425d9b2ecf5b6f7cba0505462
Peter Chan <peter1989212@gmail.com>
2021-01-28 22:59:33 +0800

    Update Bilibili.js

--- Surge/Bilibili.js
+++ Surge/Bilibili.js
@@ -14,7 +14,7 @@ Author: @NobyDa
 Surge 4.2+ 远程脚本配置 :
 ****************************
 [Script]
-Bili Region = type=http-response,pattern=https:\/\/api\.bilibili\.com\/pgc\/view\/(v\d\/)?app\/season\?access_key,requires-body=1,max-size=0,control-api=1,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/Surge/JS/Bili_Auto_Regions.js
+Bili Region = type=http-response,pattern=https:\/\/api\.bilibili\.com\/pgc\/view\/(v\d\/)?app\/season\?access_key,requires-body=1,max-size=0,control-api=1,script-path= https://raw.githubusercontent.com/arneng/Rules/master/Surge/Bilibili.js
 
 [MITM]
 hostname = api.bilibili.com
